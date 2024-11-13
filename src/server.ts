b763503f2bb7969f218568caa7d1baa3c6e4053c
alter0z <91676390+alter0z@users.noreply.github.com>
2024-11-13 16:00:22 +0700

    init

---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import express, { Application } from 'express'
+// import { routes } from './routes'
+import { logger } from './utils/logger'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+
+// DB connection
+import './utils/connection'
+
+const app: Application = express()
+const port: number = 2000
+
+// parse body request
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// cors access handler
+app.use(cors())
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', '*')
+  res.setHeader('Access-Control-Allow-Headers', '*')
+  next()
+})
+
+// routes(app)
+
+app.listen(port, () => logger.info(`Server is listening on port ${port}`))
